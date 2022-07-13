493e9b755f235643b7ee1b03e0089dc447b228cd
taitd <tai.td@citigo.net>
2022-07-13 16:18:30 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,67 +1,55 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div id="wrapper">
 
-			 {/* Section */}
-			<section>
-				<header>
-					<h1>Ví cá sấu 2 mặt</h1>
-				</header>
-				<div className="content">
-					 {/* Section */}
-						<section>
-							<header>
-								<h2>Giá: <span className="price">1.118.000</span></h2>
-								<h3>Mã sp: 1 | Nhóm: <a href={process.env.PUBLIC_URL + "#"}>Ví cá sấu</a></h3>
-								<p>Mô tả về sản phẩm...</p>
-								<h2><a href={process.env.PUBLIC_URL + "#contact"} className="buy">Đặt mua</a></h2>
-							</header>
-							<div className="content">
-								<div className="gallery">
-									<a href={process.env.PUBLIC_URL + "/assets/images/product/z3558303688902_dc22306e950a45e8a467f63938be0c5c.jpg"}><img src={process.env.PUBLIC_URL + "/assets/images/product/thumbnail/z3558303688902_dc22306e950a45e8a467f63938be0c5c.jpg"} alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
-									<a href={process.env.PUBLIC_URL + "/assets/images/product/z3558303696525_8834ed31a743d50aa729119347f66f92.jpg"}><img src={process.env.PUBLIC_URL + "/assets/images/product/thumbnail/z3558303696525_8834ed31a743d50aa729119347f66f92.jpg"} alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
-									<a href={process.env.PUBLIC_URL + "/assets/images/product/z3558303696691_79007583a3edcf30572dda0ad31e002c.jpg"}><img src={process.env.PUBLIC_URL + "/assets/images/product/thumbnail/z3558303696691_79007583a3edcf30572dda0ad31e002c.jpg"} alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
-									<a href={process.env.PUBLIC_URL + "/assets/images/product/z3558303696765_01fbcd8b71a1b87a9c607f42c1dbdca0.jpg"}><img src={process.env.PUBLIC_URL + "/assets/images/product/thumbnail/z3558303696765_01fbcd8b71a1b87a9c607f42c1dbdca0.jpg"} alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
-									<a href={process.env.PUBLIC_URL + "/assets/images/product/z3558303696903_f32776726295d1772e69b48e29782a91.jpg"}><img src={process.env.PUBLIC_URL + "/assets/images/product/thumbnail/z3558303696903_f32776726295d1772e69b48e29782a91.jpg"} alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
-									<a href={process.env.PUBLIC_URL + "/assets/images/product/z3558303697034_ad03d895db8d2016849005843501e294.jpg"}><img src={process.env.PUBLIC_URL + "/assets/images/product/thumbnail/z3558303697034_ad03d895db8d2016849005843501e294.jpg"} alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
+	const [siteInfo, setSiteInfo] = useState({})
+	const [products, setProducts] = useState([])
+
+
+	useEffect(() => {
+		
+		async function fetchData() {
+			const { data } = await axios.get(process.env.PUBLIC_URL+'/data.json')
+			setSiteInfo(data.siteInfo)
+			setProducts(data.products)
+			console.log(data)
+		}
+		fetchData();
+
+	}, [])
+
+
+	return (
+		<div id="wrapper">
+
+			{products.map(product => (
+				<section key={product.id}>
+					<header>
+						<h1>{product.title}</h1>
+					</header>
+					<div className="content">
+							<section>
+								<header>
+									<h2>Giá: <span className="price">{product.priceInfo.price}</span></h2>
+									<h3>Mã sp: {product.id}<br/>Nhóm: <a href={process.env.PUBLIC_URL + "#"}>{product.category}</a></h3>
+									<p>{product.description}</p>
+									<h2><a href={process.env.PUBLIC_URL + "#contact"} className="buy">Đặt mua</a></h2>
+								</header>
+								<div className="content">
+									<div className="gallery">
+										{product.images.map((image, index) => (
+											<a key={index} href={process.env.PUBLIC_URL + `/assets/images/product/${image}`}><img src={process.env.PUBLIC_URL + `/assets/images/product/thumbnail/${image}`} alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>										
+										))}										
+									</div>
 								</div>
-							</div>
-						</section>
+							</section>
 
-				</div>
-			</section>
-			
-			<section>
-				<header>
-					<h1>Ví cá sấu 2 mặt</h1>
-				</header>
-				<div className="content">
-					 {/* Section */}
-						<section>
-							<header>
-								<h2>Giá: <span className="price">1.118.000</span></h2>
-								<h3>Mã sp: 1 | Nhóm: <a href={process.env.PUBLIC_URL + "#"}>Ví cá sấu</a></h3>
-								<p>Mô tả về sản phẩm...</p>
-								<h2><a href={process.env.PUBLIC_URL + "#contact"} className="buy">Đặt mua</a></h2>
-							</header>
-							<div className="content">
-								<div className="gallery">
-									<a href={process.env.PUBLIC_URL + "/assets/images/product/z3558304365106_e2127fe6732f8878c6fd0242ae91f78f.jpg"}><img src={process.env.PUBLIC_URL + "/assets/images/product/thumbnail/z3558304365106_e2127fe6732f8878c6fd0242ae91f78f.jpg"} alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
-									<a href={process.env.PUBLIC_URL + "/assets/images/product/z3558304370739_7c71feadbc25f002889f84b5d8ce011c.jpg"}><img src={process.env.PUBLIC_URL + "/assets/images/product/thumbnail/z3558304370739_7c71feadbc25f002889f84b5d8ce011c.jpg"} alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
-									<a href={process.env.PUBLIC_URL + "/assets/images/product/z3558304370794_e268adb241b336b9f2f3e557ecb2bdc9.jpg"}><img src={process.env.PUBLIC_URL + "/assets/images/product/thumbnail/z3558304370794_e268adb241b336b9f2f3e557ecb2bdc9.jpg"} alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
-									<a href={process.env.PUBLIC_URL + "/assets/images/product/z3558304370829_70dc71f943f8948b03fcfa0a2fdde76a.jpg"}><img src={process.env.PUBLIC_URL + "/assets/images/product/thumbnail/z3558304370829_70dc71f943f8948b03fcfa0a2fdde76a.jpg"} alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
-									<a href={process.env.PUBLIC_URL + "/assets/images/product/z3558304378635_1ca8b6eaf1d565b8d82393fc45ee06ec.jpg"}><img src={process.env.PUBLIC_URL + "/assets/images/product/thumbnail/z3558304378635_1ca8b6eaf1d565b8d82393fc45ee06ec.jpg"} alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
-								</div>
-							</div>
-						</section>
+					</div>
+				</section> 
+			))}
 
-				</div>
-			</section>
-
-			 {/* Section */}
 			<section id="contact">
 				<header>
 					<h2>Liên hệ</h2>
@@ -69,11 +57,11 @@ function App() {
 					<p>Email: xuongvitruongchinh@gmail.com</p>
 				</header>
 				<div className="content">
-        <p><strong>Chúng tôi</strong> có xưởng trực tiếp sản xuất các sản phẩm đồ da cá sấu/da bò cao cấp thật 100% (cam kết đền gấp 10 giá trị đơn hàng nếu phát hiện sản phẩm không phải da thật).<br/>
-- Chúng tôi sản xuất nhiều loại sản phẩm.<br/>
-- Chúng tôi nhận gia công cho khách hàng có nhu cầu đặt hàng theo thương hiệu riêng, sản xuất mẫu mã theo yêu cầu.<br/>
-- Chúng tôi có đội ngũ công nhân tay nghề cao có nhiều năm kinh nghiệm.<br/>
-Hãy liên hệ với chúng tôi để được phục vụ!</p>
+				<p><strong>Chúng tôi</strong> có xưởng trực tiếp sản xuất các sản phẩm đồ da cá sấu/da bò cao cấp thật 100% (cam kết đền gấp 10 giá trị đơn hàng nếu phát hiện sản phẩm không phải da thật).<br/>
+	- Chúng tôi sản xuất nhiều loại sản phẩm.<br/>
+	- Chúng tôi nhận gia công cho khách hàng có nhu cầu đặt hàng theo thương hiệu riêng, sản xuất mẫu mã theo yêu cầu.<br/>
+	- Chúng tôi có đội ngũ công nhân tay nghề cao có nhiều năm kinh nghiệm.<br/>
+	Hãy liên hệ với chúng tôi để được phục vụ!</p>
 					<form>
 						<div className="fields">
 							<div className="field half">
@@ -107,13 +95,13 @@ Hãy liên hệ với chúng tôi để được phục vụ!</p>
 				</footer>
 			</section>
 
-			 {/* Copyright */}
+				{/* Copyright */}
 			<div className="copyright">&copy; All rights reserved.</div>
 			
 			<button  id="toTop" title="Go to top">Top</button>
 
 		</div>
-  );
+	);
 }
 
 export default App;
