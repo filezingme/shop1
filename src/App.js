24719d7a8cdac32f0fea002a02d964f9093fe08f
taitd <tai.td@citigo.net>
2022-07-13 13:28:08 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,116 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="wrapper">
+
+			 {/* Section */}
+			<section>
+				<header>
+					<h1>Ví cá sấu 2 mặt</h1>
+				</header>
+				<div className="content">
+					 {/* Section */}
+						<section>
+							<header>
+								<h2>Giá: <span className="price">1.118.000</span></h2>
+								<h3>Mã sp: 1 | Nhóm: <a href="#">Ví cá sấu</a></h3>
+								<p>Mô tả về sản phẩm...</p>
+								<h2><a href="#contact" className="buy">Đặt mua</a></h2>
+							</header>
+							<div className="content">
+								<div className="gallery">
+									<a href="/assets/images/product/z3558303688902_dc22306e950a45e8a467f63938be0c5c.jpg"><img src="/assets/images/product/thumbnail/z3558303688902_dc22306e950a45e8a467f63938be0c5c.jpg" alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
+									<a href="/assets/images/product/z3558303696525_8834ed31a743d50aa729119347f66f92.jpg"><img src="/assets/images/product/thumbnail/z3558303696525_8834ed31a743d50aa729119347f66f92.jpg" alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
+									<a href="/assets/images/product/z3558303696691_79007583a3edcf30572dda0ad31e002c.jpg"><img src="/assets/images/product/thumbnail/z3558303696691_79007583a3edcf30572dda0ad31e002c.jpg" alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
+									<a href="/assets/images/product/z3558303696765_01fbcd8b71a1b87a9c607f42c1dbdca0.jpg"><img src="/assets/images/product/thumbnail/z3558303696765_01fbcd8b71a1b87a9c607f42c1dbdca0.jpg" alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
+									<a href="/assets/images/product/z3558303696903_f32776726295d1772e69b48e29782a91.jpg"><img src="/assets/images/product/thumbnail/z3558303696903_f32776726295d1772e69b48e29782a91.jpg" alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
+									<a href="/assets/images/product/z3558303697034_ad03d895db8d2016849005843501e294.jpg"><img src="/assets/images/product/thumbnail/z3558303697034_ad03d895db8d2016849005843501e294.jpg" alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
+								</div>
+							</div>
+						</section>
+
+				</div>
+			</section>
+			
+			<section>
+				<header>
+					<h1>Ví cá sấu 2 mặt</h1>
+				</header>
+				<div className="content">
+					 {/* Section */}
+						<section>
+							<header>
+								<h2>Giá: <span className="price">1.118.000</span></h2>
+								<h3>Mã sp: 1 | Nhóm: <a href="#">Ví cá sấu</a></h3>
+								<p>Mô tả về sản phẩm...</p>
+								<h2><a href="#contact" className="buy">Đặt mua</a></h2>
+							</header>
+							<div className="content">
+								<div className="gallery">
+									<a href="/assets/images/product/z3558304365106_e2127fe6732f8878c6fd0242ae91f78f.jpg"><img src="/assets/images/product/thumbnail/z3558304365106_e2127fe6732f8878c6fd0242ae91f78f.jpg" alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
+									<a href="/assets/images/product/z3558304370739_7c71feadbc25f002889f84b5d8ce011c.jpg"><img src="/assets/images/product/thumbnail/z3558304370739_7c71feadbc25f002889f84b5d8ce011c.jpg" alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
+									<a href="/assets/images/product/z3558304370794_e268adb241b336b9f2f3e557ecb2bdc9.jpg"><img src="/assets/images/product/thumbnail/z3558304370794_e268adb241b336b9f2f3e557ecb2bdc9.jpg" alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
+									<a href="/assets/images/product/z3558304370829_70dc71f943f8948b03fcfa0a2fdde76a.jpg"><img src="/assets/images/product/thumbnail/z3558304370829_70dc71f943f8948b03fcfa0a2fdde76a.jpg" alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
+									<a href="/assets/images/product/z3558304378635_1ca8b6eaf1d565b8d82393fc45ee06ec.jpg"><img src="/assets/images/product/thumbnail/z3558304378635_1ca8b6eaf1d565b8d82393fc45ee06ec.jpg" alt="Click để xem ảnh rõ nét" title="Click để xem ảnh rõ nét" /></a>
+								</div>
+							</div>
+						</section>
+
+				</div>
+			</section>
+
+			 {/* Section */}
+			<section id="contact">
+				<header>
+					<h2>Liên hệ</h2>
+					<h3>Điện Thoại: 0971265356</h3>
+					<p>Email: xuongvitruongchinh@gmail.com</p>
+				</header>
+				<div className="content">
+        <p><strong>Chúng tôi</strong> có xưởng trực tiếp sản xuất các sản phẩm đồ da cá sấu/da bò cao cấp thật 100% (cam kết đền gấp 10 giá trị đơn hàng nếu phát hiện sản phẩm không phải da thật).<br/>
+- Chúng tôi sản xuất nhiều loại sản phẩm.<br/>
+- Chúng tôi nhận gia công cho khách hàng có nhu cầu đặt hàng theo thương hiệu riêng, sản xuất mẫu mã theo yêu cầu.<br/>
+- Chúng tôi có đội ngũ công nhân tay nghề cao có nhiều năm kinh nghiệm.<br/>
+Hãy liên hệ với chúng tôi để được phục vụ!</p>
+					<form>
+						<div className="fields">
+							<div className="field half">
+								<input type="text" name="name" id="name" placeholder="Tên" />
+							</div>
+							<div className="field half">
+								<input type="email" name="email" id="email" placeholder="Email" />
+							</div>
+							<div className="field">
+								<textarea name="message" id="message" placeholder="Thông tin liên hệ" rows="7"></textarea>
+							</div>
+						</div>
+						<ul className="actions">
+							<li><input type="submit" value="Gửi" className="button primary" /></li>
+						</ul>
+					</form>
+				</div>
+				<footer>
+					<ul className="items">
+						<li>
+							<ul className="icons">
+								<li><a href="#" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
+								<li><a href="#" className="icon brands fa-facebook-f"><span className="label">Facebook</span></a></li>
+								<li><a href="#" className="icon brands fa-instagram"><span className="label">Instagram</span></a></li>
+								<li><a href="#" className="icon brands fa-linkedin-in"><span className="label">LinkedIn</span></a></li>
+								<li><a href="#" className="icon brands fa-github"><span className="label">GitHub</span></a></li>
+								<li><a href="#" className="icon brands fa-codepen"><span className="label">Codepen</span></a></li>
+							</ul>
+						</li>
+					</ul>
+				</footer>
+			</section>
+
+			 {/* Copyright */}
+			<div className="copyright">&copy; All rights reserved.</div>
+			
+			<button  id="toTop" title="Go to top">Top</button>
+
+		</div>
   );
 }
 
