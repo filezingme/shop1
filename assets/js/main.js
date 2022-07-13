56ab53edbbb9aceda29310a0e14db7fef65cbecb
taitd <tai.td@citigo.net>
2022-07-13 14:18:29 +0700

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,206 +3,207 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+$(document).ready(function(){
+	(function($) {
 
-(function($) {
+		var	$window = $(window),
+			$body = $('body');
 
-	var	$window = $(window),
-		$body = $('body');
-
-	// Breakpoints.
-		breakpoints({
-			default:   ['1681px',   null       ],
-			xlarge:    ['1281px',   '1680px'   ],
-			large:     ['981px',    '1280px'   ],
-			medium:    ['737px',    '980px'    ],
-			small:     ['481px',    '736px'    ],
-			xsmall:    ['361px',    '480px'    ],
-			xxsmall:   [null,       '360px'    ]
-		});
-
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
-
-	// Hack: Enable IE workarounds.
-		if (browser.name == 'ie')
-			$body.addClass('is-ie');
-
-	// Mobile?
-		if (browser.mobile)
-			$body.addClass('is-mobile');
-
-	// Scrolly.
-		$('.scrolly')
-			.scrolly({
-				offset: 100
+		// Breakpoints.
+			breakpoints({
+				default:   ['1681px',   null       ],
+				xlarge:    ['1281px',   '1680px'   ],
+				large:     ['981px',    '1280px'   ],
+				medium:    ['737px',    '980px'    ],
+				small:     ['481px',    '736px'    ],
+				xsmall:    ['361px',    '480px'    ],
+				xxsmall:   [null,       '360px'    ]
 			});
 
-	// Polyfill: Object fit.
-		if (!browser.canUse('object-fit')) {
-
-			$('.image[data-position]').each(function() {
-
-				var $this = $(this),
-					$img = $this.children('img');
-
-				// Apply img as background.
-					$this
-						.css('background-image', 'url("' + $img.attr('src') + '")')
-						.css('background-position', $this.data('position'))
-						.css('background-size', 'cover')
-						.css('background-repeat', 'no-repeat');
-
-				// Hide img.
-					$img
-						.css('opacity', '0');
-
+		// Play initial animations on page load.
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-preload');
+				}, 100);
 			});
 
-			$('.gallery > a').each(function() {
+		// Hack: Enable IE workarounds.
+			if (browser.name == 'ie')
+				$body.addClass('is-ie');
 
-				var $this = $(this),
-					$img = $this.children('img');
+		// Mobile?
+			if (browser.mobile)
+				$body.addClass('is-mobile');
 
-				// Apply img as background.
-					$this
-						.css('background-image', 'url("' + $img.attr('src') + '")')
-						.css('background-position', 'center')
-						.css('background-size', 'cover')
-						.css('background-repeat', 'no-repeat');
+		// Scrolly.
+			$('.scrolly')
+				.scrolly({
+					offset: 100
+				});
 
-				// Hide img.
-					$img
-						.css('opacity', '0');
+		// Polyfill: Object fit.
+			if (!browser.canUse('object-fit')) {
 
-			});
+				$('.image[data-position]').each(function() {
 
-		}
+					var $this = $(this),
+						$img = $this.children('img');
 
-	// Gallery.
-		$('.gallery')
-			.on('click', 'a', function(event) {
+					// Apply img as background.
+						$this
+							.css('background-image', 'url("' + $img.attr('src') + '")')
+							.css('background-position', $this.data('position'))
+							.css('background-size', 'cover')
+							.css('background-repeat', 'no-repeat');
 
-				var $a = $(this),
-					$gallery = $a.parents('.gallery'),
-					$modal = $gallery.children('.modal'),
-					$modalImg = $modal.find('img'),
-					href = $a.attr('href');
+					// Hide img.
+						$img
+							.css('opacity', '0');
 
-				// Not an image? Bail.
-					if (!href.match(/\.(jpg|gif|png|mp4)$/))
-						return;
+				});
 
-				// Prevent default.
-					event.preventDefault();
-					event.stopPropagation();
+				$('.gallery > a').each(function() {
 
-				// Locked? Bail.
-					if ($modal[0]._locked)
-						return;
+					var $this = $(this),
+						$img = $this.children('img');
 
-				// Lock.
-					$modal[0]._locked = true;
+					// Apply img as background.
+						$this
+							.css('background-image', 'url("' + $img.attr('src') + '")')
+							.css('background-position', 'center')
+							.css('background-size', 'cover')
+							.css('background-repeat', 'no-repeat');
 
-				// Set src.
-					$modalImg.attr('src', href);
+					// Hide img.
+						$img
+							.css('opacity', '0');
 
-				// Set visible.
-					$modal.addClass('visible');
+				});
 
-				// Focus.
-					$modal.focus();
+			}
 
-				// Delay.
-					setTimeout(function() {
+		// Gallery.
+			$('.gallery')
+				.on('click', 'a', function(event) {
 
-						// Unlock.
-							$modal[0]._locked = false;
+					var $a = $(this),
+						$gallery = $a.parents('.gallery'),
+						$modal = $gallery.children('.modal'),
+						$modalImg = $modal.find('img'),
+						href = $a.attr('href');
 
-					}, 600);
+					// Not an image? Bail.
+						if (!href.match(/\.(jpg|gif|png|mp4)$/))
+							return;
 
-			})
-			.on('click', '.modal', function(event) {
+					// Prevent default.
+						event.preventDefault();
+						event.stopPropagation();
 
-				var $modal = $(this),
-					$modalImg = $modal.find('img');
+					// Locked? Bail.
+						if ($modal[0]._locked)
+							return;
 
-				// Locked? Bail.
-					if ($modal[0]._locked)
-						return;
+					// Lock.
+						$modal[0]._locked = true;
 
-				// Already hidden? Bail.
-					if (!$modal.hasClass('visible'))
-						return;
+					// Set src.
+						$modalImg.attr('src', href);
 
-				// Stop propagation.
-					event.stopPropagation();
+					// Set visible.
+						$modal.addClass('visible');
 
-				// Lock.
-					$modal[0]._locked = true;
+					// Focus.
+						$modal.focus();
 
-				// Clear visible, loaded.
-					$modal
-						.removeClass('loaded')
-
-				// Delay.
-					setTimeout(function() {
-
-						$modal
-							.removeClass('visible')
-
+					// Delay.
 						setTimeout(function() {
-
-							// Clear src.
-								$modalImg.attr('src', '');
 
 							// Unlock.
 								$modal[0]._locked = false;
 
-							// Focus.
-								$body.focus();
+						}, 600);
 
-						}, 475);
+				})
+				.on('click', '.modal', function(event) {
 
-					}, 125);
+					var $modal = $(this),
+						$modalImg = $modal.find('img');
 
-			})
-			.on('keypress', '.modal', function(event) {
+					// Locked? Bail.
+						if ($modal[0]._locked)
+							return;
 
-				var $modal = $(this);
+					// Already hidden? Bail.
+						if (!$modal.hasClass('visible'))
+							return;
 
-				// Escape? Hide modal.
-					if (event.keyCode == 27)
-						$modal.trigger('click');
+					// Stop propagation.
+						event.stopPropagation();
 
-			})
-			.on('mouseup mousedown mousemove', '.modal', function(event) {
+					// Lock.
+						$modal[0]._locked = true;
 
-				// Stop propagation.
-					event.stopPropagation();
+					// Clear visible, loaded.
+						$modal
+							.removeClass('loaded')
 
-			})
-			.prepend('<div class="modal" tabIndex="-1"><div class="inner"><img src="" /></div></div>')
-				.find('img')
-					.on('load', function(event) {
-
-						var $modalImg = $(this),
-							$modal = $modalImg.parents('.modal');
-
+					// Delay.
 						setTimeout(function() {
 
-							// No longer visible? Bail.
-								if (!$modal.hasClass('visible'))
-									return;
+							$modal
+								.removeClass('visible')
 
-							// Set loaded.
-								$modal.addClass('loaded');
+							setTimeout(function() {
 
-						}, 275);
+								// Clear src.
+									$modalImg.attr('src', '');
 
-					});
+								// Unlock.
+									$modal[0]._locked = false;
 
-})(jQuery);
+								// Focus.
+									$body.focus();
+
+							}, 475);
+
+						}, 125);
+
+				})
+				.on('keypress', '.modal', function(event) {
+
+					var $modal = $(this);
+
+					// Escape? Hide modal.
+						if (event.keyCode == 27)
+							$modal.trigger('click');
+
+				})
+				.on('mouseup mousedown mousemove', '.modal', function(event) {
+
+					// Stop propagation.
+						event.stopPropagation();
+
+				})
+				.prepend('<div class="modal" tabIndex="-1"><div class="inner"><img src="" /></div></div>')
+					.find('img')
+						.on('load', function(event) {
+
+							var $modalImg = $(this),
+								$modal = $modalImg.parents('.modal');
+
+							setTimeout(function() {
+
+								// No longer visible? Bail.
+									if (!$modal.hasClass('visible'))
+										return;
+
+								// Set loaded.
+									$modal.addClass('loaded');
+
+							}, 275);
+
+						});
+
+	})(jQuery);
+})
