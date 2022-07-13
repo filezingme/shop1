aaa8ed5dd4ddb6a561ba5a47af77f178cfc6c765
taitd <tai.td@citigo.net>
2022-07-13 16:19:55 +0700

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-$(document).ready(function(){
+window.onload = function(e){
 	(function($) {
 
 		var	$window = $(window),
@@ -206,4 +206,4 @@ $(document).ready(function(){
 						});
 
 	})(jQuery);
-})
+}
