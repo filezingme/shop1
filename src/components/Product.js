fe87b2bbced577d95585d04a29634dd9c7ddea48
taitd <tai.td@citigo.net>
2022-07-14 09:47:19 +0700

    .

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 function Product({ products, siteInfo }) {
 
+    //console.log(process.env.PUBLIC_URL)
+
     function currencyFormat(num) {
         return num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
     }
@@ -17,7 +19,7 @@ function Product({ products, siteInfo }) {
                         <header>
                             <h2>Giá: <span className="price">{currencyFormat(product.priceInfo.price)}</span></h2>
                             <h3>Mã sp: <strong>{product.id}</strong><br/>
-                                Nhóm: <a href={`/category/${product.category}`}>{product.category}</a></h3>
+                                Nhóm: <a href={`category/${product.category}`}>{product.category}</a></h3>
                             <p>{product.description}</p>
                             <h2><a href="#" className="buy">{siteInfo.buyBtnText}</a></h2>
                         </header>
