809f003fd10ffd211be5b8f86bd25dd1f33f32f0
taitd <tai.td@citigo.net>
2022-07-18 15:57:54 +0700

    npm install --save react-lightgallery --force

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,27 @@
 import React, {useState, useEffect} from 'react'
 import { useParams } from "react-router-dom";
 import {Helmet} from "react-helmet";
+import PT from "prop-types";
+import {
+  LightgalleryProvider,
+  LightgalleryItem
+} from "react-lightgallery";
+import "lightgallery.js/dist/css/lightgallery.css"; //https://codesandbox.io/examples/package/react-lightgallery > https://codesandbox.io/s/mo45kpo92j
+
+
+const PhotoItem = ({ image, thumb, title, group }) => (
+    <div style={{ maxWidth: "33%", width: "33%", padding: "5px" }}>
+        <LightgalleryItem group={group} src={image} thumb={thumb}>
+            <img src={thumb} style={{ width: "100%" }} loading="lazy" alt={title} title={title} />
+        </LightgalleryItem>
+    </div>
+);
+PhotoItem.propTypes = {
+    image: PT.string.isRequired,
+    thumb: PT.string,
+    group: PT.string.isRequired
+};
+
 
 function Product({ data }) {
 
@@ -51,12 +72,28 @@ function Product({ data }) {
                             <h2><a href="#" className="buy">{data.siteInfo.buyBtnText}</a></h2>
                         </header>
                         <div className="content">
-                            <div className={`gallery lightgallery`}>
-                                {product.images.map((image, index) => (
+                            <div className={`gallery`}>
+                                {/* {product.images.map((image, index) => (
                                     <a key={index} data-src={process.env.PUBLIC_URL + `/assets/images/product/${image}`}>
                                         <img src={process.env.PUBLIC_URL + `/assets/images/product/thumbnail/${image}`} alt={product.title} title={product.title} loading="lazy" />
                                     </a>										
-                                ))}										
+                                ))}		 */}
+
+                                <LightgalleryProvider>
+                                    {/* {GROUP1.map((p, idx) => (
+                                    <PhotoItem key={idx} image={p[0]} thumb={p[1]} group="group1" />
+                                    ))} */}
+
+                                    {product.images.map((image, index) => (
+                                        <PhotoItem key={index} 
+                                            image={process.env.PUBLIC_URL + `/assets/images/product/${image}`}
+                                            thumb={process.env.PUBLIC_URL + `/assets/images/product/thumbnail/${image}`} 
+                                            title={product.title}
+                                            group={product.id} 
+                                        />									
+                                    ))}		
+                                    
+                                </LightgalleryProvider>								
                             </div>
                         </div>
                     </section>
