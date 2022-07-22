a46c02926e6ecaf68f264a543c5508f1e0503f78
taitd <tai.td@citigo.net>
2022-07-22 13:41:19 +0700

    Updates

---
+++ precache-manifest.8f5a983cdd022d036f309b099ba800dd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8480407f774091d4cf9",
+    "url": "/xtcbanhang.com/static/css/main.0575a6bd.chunk.css"
+  },
+  {
+    "revision": "a8480407f774091d4cf9",
+    "url": "/xtcbanhang.com/static/js/main.a8480407.chunk.js"
+  },
+  {
+    "revision": "5fbdb5cdd6feaee873f2",
+    "url": "/xtcbanhang.com/static/js/1.5fbdb5cd.chunk.js"
+  },
+  {
+    "revision": "be22076347f6609084ac",
+    "url": "/xtcbanhang.com/static/css/2.531ebba7.chunk.css"
+  },
+  {
+    "revision": "be22076347f6609084ac",
+    "url": "/xtcbanhang.com/static/js/2.be220763.chunk.js"
+  },
+  {
+    "revision": "01aaf628738836bf3434",
+    "url": "/xtcbanhang.com/static/js/runtime~main.01aaf628.js"
+  },
+  {
+    "revision": "f4292655f93dd12d9b8e4fc067ef2489",
+    "url": "/xtcbanhang.com/static/media/lg.f4292655.ttf"
+  },
+  {
+    "revision": "2ec2cb2199d4d881e6a6ad86690f6add",
+    "url": "/xtcbanhang.com/static/media/lg.2ec2cb21.svg"
+  },
+  {
+    "revision": "1fbfd4bcffccb94e8e8a5ea70616b296",
+    "url": "/xtcbanhang.com/static/media/lg.1fbfd4bc.woff"
+  },
+  {
+    "revision": "ff92fdd39aac8a0f2853f5750d17ccce",
+    "url": "/xtcbanhang.com/index.html"
+  }
+];
