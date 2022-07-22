9a6820bff0747fcc184152c3b3de7a7446f89b41
taitd <tai.td@citigo.net>
2022-07-22 09:07:14 +0700

    .

--- src/components/CategoryMenu.js
+++ src/components/CategoryMenu.js
@@ -11,10 +11,14 @@ function CategoryMenu({categoryList, handleClose, isShow}) {
             </Offcanvas.Header>
             <Offcanvas.Body>
                 <Nav className="flex-column">
-                    <Nav.Link href={process.env.PUBLIC_URL}><b>Trang chủ</b></Nav.Link>
+                    <Nav.Link href={process.env.PUBLIC_URL}>
+                        <i class="fa fa-home" aria-hidden="true"></i> <b>Trang chủ</b>
+                    </Nav.Link>
                     {categoryList.map((category, index) => (
                         <React.Fragment key={index}>
-                            <Nav.Link href={process.env.PUBLIC_URL + `/category/${category.name.toLowerCase()}`}>{category.name} ({category.count})</Nav.Link>
+                            <Nav.Link href={process.env.PUBLIC_URL + `/category/${category.name.toLowerCase()}`}>
+                                <i class='fas fa-angle-right'></i> {category.name} ({category.count})
+                            </Nav.Link>
                         </React.Fragment>
                     ))}
                 </Nav>
