6e3d7a7ba8e7290ecabfed834aa809bf273c3e4e
taitd <tai.td@citigo.net>
2022-08-01 10:02:12 +0700

    Updates

---
+++ precache-manifest.7f62cd07a015c3ee232d9672f798fb43.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "9842d05e1738013e56ca",
+    "url": "/shop1/static/css/main.6fb5e6a1.chunk.css"
+  },
+  {
+    "revision": "9842d05e1738013e56ca",
+    "url": "/shop1/static/js/main.9842d05e.chunk.js"
+  },
+  {
+    "revision": "dc4dc0021066ace78671",
+    "url": "/shop1/static/js/1.dc4dc002.chunk.js"
+  },
+  {
+    "revision": "90ace4f03b4a0c7e3875",
+    "url": "/shop1/static/css/2.e3691450.chunk.css"
+  },
+  {
+    "revision": "90ace4f03b4a0c7e3875",
+    "url": "/shop1/static/js/2.90ace4f0.chunk.js"
+  },
+  {
+    "revision": "bc2c949617b9a3e3e1a1",
+    "url": "/shop1/static/js/runtime~main.bc2c9496.js"
+  },
+  {
+    "revision": "f4292655f93dd12d9b8e4fc067ef2489",
+    "url": "/shop1/static/media/lg.f4292655.ttf"
+  },
+  {
+    "revision": "2ec2cb2199d4d881e6a6ad86690f6add",
+    "url": "/shop1/static/media/lg.2ec2cb21.svg"
+  },
+  {
+    "revision": "1fbfd4bcffccb94e8e8a5ea70616b296",
+    "url": "/shop1/static/media/lg.1fbfd4bc.woff"
+  },
+  {
+    "revision": "4fbaab0c7520775b83d9adbbc2214a6f",
+    "url": "/shop1/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/xtcbanhang.com/precache-manifest.284ad17896c561efe3396b22cbcc5faf.js"
+  "/shop1/precache-manifest.7f62cd07a015c3ee232d9672f798fb43.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/xtcbanhang.com/index.html", {
+workbox.routing.registerNavigationRoute("/shop1/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
