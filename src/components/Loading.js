94386469aa965463692528459aeee76772e8c7d2
taitd <tai.td@citigo.net>
2022-08-02 10:56:16 +0700

    Updates

--- src/components/Loading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Loading() {
-  return (
-    <img id="imgloading" src={process.env.PUBLIC_URL + `/assets/images/loading.gif`} title="loading" alt="loading" />
-  )
-}
-
-export default Loading
