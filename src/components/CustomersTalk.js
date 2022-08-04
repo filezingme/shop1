839f2ddf92705d4b6515290a1f366cb40755c300
taitd <tai.td@citigo.net>
2022-08-04 19:58:00 +0700

    .

--- src/components/CustomersTalk.js
+++ src/components/CustomersTalk.js
@@ -26,7 +26,7 @@ function CustomersTalk({customersTalkList, handleClose, isShow, handleShowLightG
             </Modal.Header>
             <Modal.Body>                
                 {customersTalkList.map((item, index) => (
-                    <div key={index} className="card mb-3">
+                    <div key={index} className="card mb-4">
                         <div className="row no-gutters">
                             <div className="col-lg-5 col-md-5 col-xs-6">
                                 <div className="customer_photos">
