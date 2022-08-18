1d0794cc92f63497f99cf16a2573a4f153c11e27
taitd <tai.td@citigo.net>
2022-08-19 00:58:01 +0700

    update

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -4,8 +4,10 @@ import ReactAudioPlayer from 'react-audio-player';
 function AudioPlayer() {
   return (
     <div className='audio-player'>
-        <ReactAudioPlayer autoPlay controls
+        <ReactAudioPlayer controls
             src={process.env.PUBLIC_URL+'/media/Wo-Men-Bu-Yi-Yang.mp3'}
+            autoPlay={true}
+            onPlay={e => console.log("AudioPlayer: onPlay")}
         />
     </div>
   )
